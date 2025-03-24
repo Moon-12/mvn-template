cb8da7a167349b265395e4b8095b99e5ebae0767
Moon-12 <ashwijanayak@gmail.com>
2025-03-24 00:06:24 -0500

    teacher submenus added

--- src/main/java/com/ashwija/mvn/dao/TeacherDao.java
+++ src/main/java/com/ashwija/mvn/dao/TeacherDao.java
@@ -3,20 +3,34 @@ package com.ashwija.mvn.dao;
 import com.ashwija.mvn.model.TeacherEntity;
 
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TeacherDao extends AppDao<TeacherEntity> {
     @Override
     String getInsertSql() {
-        return "";
+
+        return "Insert into teacher (name) VALUES(?)";
     }
 
     @Override
     String getFetchAllSql() {
-        return "";
+        return "select * from teacher where active=1";
     }
 
     @Override
     TeacherEntity getEntityFromResultSet(ResultSet resultSet) {
-        return null;
+        try {
+            return new TeacherEntity(resultSet.getInt("id"), resultSet.getString("name"));
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public List<TeacherEntity> fetchAll() {
+        List<TeacherEntity> teacherEntities = new ArrayList<>();
+        teacherEntities = super.fetchAll();
+        return teacherEntities;
     }
 }
